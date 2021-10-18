64b238de76a38bc69fe11497450e9cfab1bb7fc9
Lapi <vojtechlapunik@gmail.com>
2021-10-18 15:25:28 +0200

    Vymazal jsem speciální regularizovanou funkci pro polynomiální proklad, místo toho jsem jen upravil funkci bez regularizace o parametr lambda, který lze zadat nenulový pro právě docílení regularizace. Dál taky testuju, jestli se bude ignorovat soubor .asv, jelikož jsem vytvořil gitignore soubor.

--- Polynomial_regression.m
+++ Polynomial_regression.m
@@ -1,8 +1,12 @@
-function [t_w,w,e,E_w] = Polynomial_regression(x,t,M) 
-%   x:      vzorkovací čas
-%   t:      naměřenné vzorky
-%   M:      řád polynomu
-%   t_w:    aproximace  
+function [t_w,w,e,E_w] = Polynomial_regression(x,t,M,lambda) 
+%   x:          vzorkovací čas
+%   t:          naměřenné vzorky
+%   M:          řád polynomu
+%   lambda:     regularizační člen
+%   t_w:        body aproximace pro vstupní hodnoty x
+%   w:          koeficienty aproximačního polynomu
+%   e:          rozdíl bodů aproximační funkce a naměřených výstupů
+%   E_w:        velikost druhých mocnin rozdílů e (metoda nejmenších čtverců)
 
 N = length(x);          % počet pozorování
 
@@ -12,10 +16,13 @@ Phi_T_x_t = zeros(1,M+1);       % matice (Phi'*t), kde t je vektor výstupů
 for j = 0:M
     for i = 0:M
         if(i == 0 && j == 0)
-            Phi_T_x_Phi(i+1,j+1) = N;
+            Phi_T_x_Phi(i+1,j+1) = N+lambda;
             continue;
-        else
+        else    
             Phi_T_x_Phi(i+1,j+1) = sum(x.^(i+j)); % sestavení matice (Phi'*Phi), kde phi_m = w_m*x^m  
+            if(i == j)
+                Phi_T_x_Phi(i+1,j+1) = Phi_T_x_Phi(i+1,j+1) + lambda;
+            end
         end
     end
     Phi_T_x_t(j+1,1) = sum((x.^(j)).*t); 
@@ -25,7 +32,7 @@ w = Phi_T_x_Phi\Phi_T_x_t;      % koeficienty vypočeteného polynomu
 
 t_w = Polynomial_evaluation(x,w);
 
-e = (t - t_w);           % odchylky od naměřených dat
+e = (t - t_w);              % odchylky od naměřených dat
 E_w = (1/2)*(sum(e.^2));    % suma z odchylek na druhou
 
 end
